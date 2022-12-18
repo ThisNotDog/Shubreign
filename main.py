15a40ccb735a851ae117c37f24fd3d6bfac7c79d
ThisNotDog <111262255+ThisNotDog@users.noreply.github.com>
2022-12-18 15:47:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import random
 import os
 import re
-
+import time
+from events import get_random_event_I
 #rogue-like rpg
 
-#player class holds attributes and amount of gold
+#player class holds attributes, amount of gold, and current day
 class Player:
     def __init__(self, name, hp, attack, defense, agility, day, gold):
         self.name = name
@@ -25,31 +26,61 @@ class Player:
  
 #monster class holds enemy stats               
 class Monster:
-    def __init__(self, name, hp, hit_chance, dodge_chance):
+    def __init__(self, name, hp, attack, hit_chance, dodge_chance):
         self.name = name
         self.hp = hp
         self.hit_chance = hit_chance
         self.dodge_chance = dodge_chance
-  
-user= Player("", 10, 1, 1, 1, 0, 20)
-run = True
-def main():
-    input("welcome to the tyranic peninsula, what is your name?\n")
-    user.name = input
-    print("pretty stupid name or whatever, let's just start\n".format(user.name))
-    while run == True and user.hp > 0:
-        i = range(1,11)
-        if i == 1 or i == 4 or i == 6:
-            get_fight_I
-        if i == 2 or i == 7 or i == 8:
-            get_random_event_I
-        if i == 3:
-            get_dungeon_I
-        if i == 5 or i == 9:
-            get_shop_I
-        if i == 10:
-            boss_I
+        self.attack = attack
+#player object that holds stats, day, and gold
+user= Player("", 10, 1, 1, 1, 1, 20)
+#monster objects for fight events
+dargen = Monster("dargen", 6, 1, .50, .30)
+
+#fight scene mechanics, takes in monster and player objects
+def get_fight_I(user, monster):
+    print('you see a {}, are you prepared to fight?'.format(monster.name))
+    i = input('press y to fight or n to run away\n')
+    if i == "y":
+        while user.hp > 0 and monster.hp > 0:
+            player_hit = random.randint(user.attack, user.attack+2)
+            monster_hit = random.randint(monster.attack, monster.attack+1)
+            print('you swing your weapon at the monster')
+            monster.hp -= player_hit
+            time.sleep(1)
+            print('the monster attacks you')
+            user.hp -= monster_hit
+        if monster.hp <= 0:
+            print('you have vanquished the monster and find coins in their pockets')
+            user.gold += 20
+            user.day += 1
+        if user.hp <= 0:
+            print('you have died, game over')
+            run = False
+    else:
+        user.day += 1
             
+        
+def main():
+    start = input("welcome to the tyranic peninsula, what is your name?\n")
+    user.name = start
+    print("hmm...{}, pretty stupid name or whatever, let's just start\n".format(user.name))
+    run = True
+    while run == True and user.hp > 0:
+        
+        if user.day == 1 or user.day == 4 or user.day == 6:
+            get_fight_I(user, dargen)
+            print(user.gold)
+        if user.day == 2 or user.day == 7 or user.day == 8:
+            get_random_event_I(user)
+        if user.day == 3:
+            get_dungeon_I
+        if user.day == 5 or user.day == 9:
+            get_shop_I
+        if user.day == 10:
+            boss_I
+    run = False
+main()   
         
         
         

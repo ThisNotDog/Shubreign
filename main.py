f2e36f10fedb9cfffafa52c0c47138651376f54d
ThisNotDog <111262255+ThisNotDog@users.noreply.github.com>
2022-12-20 01:26:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ import os
 import re
 import time
 from events import *
+
+
 #rogue-like rpg
 
-#player class holds attributes, amount of gold, and current day
+#player class holds attributes and amount of gold
 class Player:
-    def __init__(self, name, hp, attack, defense, agility, day, gold):
+    def __init__(self, name, hp, attack, defense, agility, day, gold, current_hp):
         self.name = name
-        self.hp = hp #player's max health points
-        self.attack = attack #player's attack potential
-        self.defense = defense #player's damage mitigation
-        self.agility = agility #player's chance to dodge
-        self.day = day #director of game flow
-        self.gold = gold #currency for shop purchases
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.agility = agility
+        self.day = day
+        self.gold = gold
+        self.current_hp = current_hp
     
     def __repr__(self):
         return '''your name is {}\n
@@ -23,7 +26,12 @@ class Player:
                   your agility is: {}\n
                   your gold is: {}\n
                 '''.format(name, hp, attack, defense, agility, gold)
- 
+class Items(Player):
+    def __init__(self, weapon, shield, consumable):
+        self.weapon = weapon
+        self.shield = shield
+        self.consumable = consumable
+        
 #monster class holds enemy stats               
 class Monster:
     def __init__(self, name, hp, attack, hit_chance, dodge_chance):
@@ -32,12 +40,12 @@ class Monster:
         self.hit_chance = hit_chance
         self.dodge_chance = dodge_chance
         self.attack = attack
-#player object
-user= Player("", 10, 1, 1, 1, 1, 20)
+#player object that holds stats, day, gold, and current hp
+user = Player("", 10, 1, 1, 1, 1, 20, 10)
 #monster objects for fight events
 dargen = Monster("dargen", 6, 1, .50, .30)
 
-#fight scene mechanics, takes in monster and player objects
+
 def get_fight_I(user, monster):
     print('you see a {}, are you prepared to fight?'.format(monster.name))
     i = input('press y to fight or n to run away\n')
@@ -60,22 +68,22 @@ def get_fight_I(user, monster):
     else:
         user.day += 1
             
-#main game loop, picks from either a random event, fight scene, dungeon, shop depending on the player's day class variable
-#runs until the player's hp reaches zero
+        
 def main():
     start = input("welcome to the tyranic peninsula, what is your name?\n")
     user.name = start
     print("hmm...{}, pretty stupid name or whatever, let's just start\n".format(user.name))
     run = True
     while run == True and user.hp > 0:
-        if user.day == 1 or user.day == 4 or user.day == 6:
+        
+        if user.day in (1, 4, 6):
             get_fight_I(user, dargen)
             print(user.gold)
-        if user.day == 2 or user.day == 7 or user.day == 8:
+        if user.day in (2, 7, 8):
             get_random_event_I(user)
         if user.day == 3:
             get_dungeon_I
-        if user.day == 5 or user.day == 9:
+        if user.day in (5, 9):
             get_shop_I
         if user.day == 10:
             boss_I

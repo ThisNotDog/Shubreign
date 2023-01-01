c8258ad7e598f7ee81b4195c1b48bb411a8cb155
Tyler Strate <tylerstrate@proton.me>
2023-01-01 00:08:41 -0500

    changed frog event and general structuring

--- main.py
+++ main.py
@@ -10,23 +10,22 @@ from dungeon import *
 
 #player class holds attributes and amount of gold
 class Player:
-    def __init__(self, name, hp, attack, defense, agility, day, current_hp):
+    def __init__(self, name, hp, attack, defense, day, current_hp):
         self.name = name
         self.hp = hp
         self.attack = attack
         self.defense = defense
-        self.agility = agility
         self.day = day
         self.current_hp = current_hp
     
     def __repr__(self):
-        return '''your name is {}\n
-                  your attack is: {}\n
-                  your defense is: {}\n
-                  your agility is: {}\n
-                  your gold is: {}\n
-                '''.format(name, hp, attack, defense, agility, gold)
-class Items():
+        return '''
+               your name is {}\n
+               your attack is: {}\n
+               your defense is: {}\n
+               your gold is: {}\n
+               '''.format(name, hp, attack, defense, gold)
+class Item:
     def __init__(self, weapon, shield, consumable, gold):
         self.weapon = weapon
         self.shield = shield
@@ -35,7 +34,7 @@ class Items():
         
 #monster class holds enemy stats               
 class Monster:
-    def __init__(self, name, hp, attack, hit_chance, dodge_chance):
+    def __init__(self, name, hp, attack):
         self.name = name
         self.hp = hp
         self.hit_chance = hit_chance
@@ -43,41 +42,40 @@ class Monster:
         self.attack = attack
 
 #monster object for fight events
-dargen = Monster("dargen", 6, 1, .50, .30)
+dargen_monster = Monster("dargen", 6, 1)
 
 def get_fight_I(user, monster):
     print('you see a {}, are you prepared to fight?'.format(monster.name))
     
     i = input('press y to fight or n to run away\n')
     if i == "y":
-        while user.hp > 0 and monster.hp > 0:
-            player_hit = random.randint(user.attack, user.attack+2)
-            monster_hit = random.randint(monster.attack, monster.attack+1)
+        while user.current_hp > 0 and monster.hp > 0:
+            player_hit = random.randint(0, user.attack)
+            monster_hit = random.randint(0, monster.attack)
             print('you swing your weapon at the monster')
             monster.hp -= player_hit
-            time.sleep(1)
+            print('you have hit the monster for {} damage'.format(player_hit))
+            if monster.hp <= 0:
+                print('you have vanquished the monster and find coins in their pockets')
+                user.gold += 20
+                user.day += 1
             print('the monster attacks you')
-            user.hp -= monster_hit
-        if monster.hp <= 0:
-            print('you have vanquished the monster and find coins in their pockets')
-            user.gold += 20
-            user.day += 1
-        if user.hp <= 0:
-            print('you have died, game over')
-            run = False
+            user.current_hp -= monster_hit
+            print('you have been hit for {} damage'.format(monster_hit))
+            if user.hp <= 0:
+                print('you have died, game over')
+                run = False
     else:
         user.day += 1
             
 user = Player("", 10, 1, 1, 1, 1, 20, 10)
 user_items = Items("", "", "")
-get_shop_I(user)
 def main():
-    app = QApplication(sys.argv)
-    w = MainWindow()
-    w.show()
-    app.exec()
     start = input("welcome to the tyranic peninsula, what is your name?\n")
-    user.name = start
+    if start != None:
+        user.name == start
+    if start == None:
+        user.name == "idiot"
     print("hmm...{}, pretty stupid name or whatever, let's just start\n".format(user.name))
     run = True
     while run == True and user.hp > 0:
